ee0ad4e95bb20d6a08498dc4b1e0bd85ed31d6d7
wasja1982 <wasja@yandex.ru>
2013-11-25 14:09:38 +0300

    Добавлена поддержка $_SERVER['HTTP_X_FORWARDED_PROTO'].
    При использовании перед Apache в качестве frontend сервера nginx необходимо добавить в его конфигурацию строку
    proxy_set_header X-Forwarded-Proto $scheme;

--- config/config.php
+++ config/config.php
@@ -15,6 +15,7 @@ $sUrl = Config::Get('path.root.web');
 
 $bHttps = ((isset($_SERVER['HTTP_SCHEME']) && strtolower($_SERVER['HTTP_SCHEME']) == 'https') ||
            (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off') ||
+           (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && strtolower($_SERVER['HTTP_X_FORWARDED_PROTO']) == 'https') ||
            $_SERVER['SERVER_PORT'] == 443);
 
 Config::Set('path.root.web', ($bHttps ? str_replace('http://', 'https://', $sUrl) : str_replace('https://', 'http://', $sUrl)));

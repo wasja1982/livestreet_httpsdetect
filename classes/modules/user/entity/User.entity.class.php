dce16423a45dd1a68e4016df00c359bc39948ea6
wasja1982 <wasja@yandex.ru>
2013-12-08 17:16:58 +0300

    Добавлена обработка изображений в информации пользователей "О себе" (только для изображений с текущего сервера).

--- classes/modules/user/entity/User.entity.class.php
+++ classes/modules/user/entity/User.entity.class.php
@@ -10,21 +10,29 @@
  **/
 
 class PluginHttpsdetect_ModuleUser_EntityUser extends PluginHttpsdetect_Inherit_ModuleUser_EntityUser {
-	/**
-	 * Возвращает полный веб путь до аватра
-	 *
-	 * @return string|null
-	 */
-	public function getProfileAvatar() {
+    /**
+     * Возвращает информацию о себе
+     *
+     * @return string|null
+     */
+    public function getProfileAbout() {
+        return PluginHttpsdetect::CorrectImages(parent::getProfileAbout());
+    }
+    /**
+     * Возвращает полный веб путь до аватра
+     *
+     * @return string|null
+     */
+    public function getProfileAvatar() {
         return PluginHttpsdetect::CorrectUrl(parent::getProfileAvatar());
-	}
-	/**
-	 * Возвращает полный веб путь до фото
-	 *
-	 * @return string|null
-	 */
-	public function getProfileFoto() {
+    }
+    /**
+     * Возвращает полный веб путь до фото
+     *
+     * @return string|null
+     */
+    public function getProfileFoto() {
         return PluginHttpsdetect::CorrectUrl(parent::getProfileFoto());
-	}
+    }
 }
 ?>

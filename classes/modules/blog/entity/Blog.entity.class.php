4152cbe74902a661b4d30dd732f642f6516d7a92
wasja1982 <wasja@yandex.ru>
2013-12-09 13:13:28 +0300

    Добавлено исправление ссылок на аватары блогов.

--- classes/modules/blog/entity/Blog.entity.class.php
+++ classes/modules/blog/entity/Blog.entity.class.php
@@ -10,13 +10,30 @@
  **/
 
 class PluginHttpsdetect_ModuleBlog_EntityBlog extends PluginHttpsdetect_Inherit_ModuleBlog_EntityBlog {
-	/**
-	 * Возвращает описание блога
-	 *
-	 * @return string|null
-	 */
-	public function getDescription() {
+    /**
+     * Возвращает описание блога
+     *
+     * @return string|null
+     */
+    public function getDescription() {
         return PluginHttpsdetect::CorrectImages(parent::getDescription());
-	}
+    }
+    /**
+     * Возвращает полный серверный путь до аватара блога
+     *
+     * @return string|null
+     */
+    public function getAvatar() {
+        return PluginHttpsdetect::CorrectUrl(parent::getAvatar());
+    }
+    /**
+     * Возвращает полный серверный путь до аватара блога определенного размера
+     *
+     * @param int $iSize	Размер аватара
+     * @return string
+     */
+    public function getAvatarPath($iSize=48) {
+        return PluginHttpsdetect::CorrectUrl(parent::getAvatarPath($iSize));
+    }
 }
 ?>
